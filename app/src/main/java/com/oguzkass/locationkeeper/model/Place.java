3244512f0bdc071052af63355e63c50ba45d1af1
oguzkas <oguzgkas@gmail.com>
2023-03-23 02:48:59 +0300

    finished location-keeper

---
+++ app/src/main/java/com/oguzkass/locationkeeper/model/Place.java
@@ -0,0 +1,30 @@
+package com.oguzkass.locationkeeper.model;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.io.Serializable;
+
+@Entity
+public class Place implements Serializable {
+
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+    @ColumnInfo(name = "name")
+    public String name;
+    @ColumnInfo(name ="latitude")
+    public double latitude;
+    @ColumnInfo(name = "longitude")
+    public double longitude;
+
+    public Place(String name, Double latitude, Double longitude) {
+
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+
+    }
+
+
+}
